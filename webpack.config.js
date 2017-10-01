d68ef999f3d4657acf12dc932548b4c4c3214a49
Naomi Hauret <naomi@troopers.email>
2017-10-01 17:07:18 +0200

    :rocket: deploy

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   entry: ["./main.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "./",
     filename: "assets/scripts/[name].bundle.dev.js"
   },
   module: {
