11de424db3722be120fa128482590b1cedf363d6
Naomi Hauret <naomi@troopers.email>
2017-10-03 00:09:24 +0200

    :lipstick: :sparkles: wip: creating UI with react

---
+++ src/js/components/UserInterface.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const UserInterface = () => <div>Hello UI</div>
+
+export default UserInterface
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import styles from "./styles/main.css"
 import backgroundMusic from "./assets/sound/pallet-town.mp3"
-import Scene from './js/Scene'
+import Scene from "./js/Scene"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import UserInterface from "./js/components/UserInterface"
 
 // scene
 const scene = new Scene(false, false)
+
+// UI
+ReactDOM.render(<UserInterface />, document.querySelector("#ui"));
 
 // music
 const music = new Audio(backgroundMusic)
